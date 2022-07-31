5bff2e01cd8d21c39984cd29f67096666b5fa447
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-07-31 17:25:55 +0530

    implemented uc3 code

--- uc1/uc1/Program.cs
+++ uc1/uc1/Program.cs
@@ -7,27 +7,32 @@ namespace uc1
         static void Main(string[] args)
         {
 
-            int empcheck;
-            int Emp_Rate_per_hour = 20;
+
+            int Is_part_time = 1;
+            int Is_Full_Time = 2;
             int empHrs = 0;
             int empWage = 0;
-
-            int Is_Full_Time = 1;
+            int Emp_Rate_Per_hour = 20;
             Random random = new Random();
-            empcheck = random.Next(0, 2);
+            int empcheck = random.Next(0, 2);
+
+
 
             if (empcheck == Is_Full_Time)
             {
                 empHrs = 8;
-                Console.WriteLine("Employyee is present");
+            }
+            else if (empcheck == Is_part_time)
+            {
+                empHrs = 4;
             }
             else
             {
 
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
 
             }
-            empWage = empHrs * Emp_Rate_per_hour;
+            empWage = empHrs * Emp_Rate_Per_hour;
             Console.WriteLine("empwage is :" + empWage);
 
         }
