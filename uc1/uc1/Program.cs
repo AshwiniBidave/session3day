6dd4800d778b6b142802b680a995b58751210a03
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-07-31 17:38:08 +0530

    implemented uc5 code

--- uc1/uc1/Program.cs
+++ uc1/uc1/Program.cs
@@ -8,29 +8,32 @@ namespace uc1
         public const int Is_Part_Time = 1;
         public const int Is_Full_Time = 2;
         public const int Emp_Rate_Per_Hour = 20;
-
+        public const int Num_Of_Working_Days = 20;
         static void Main(string[] args)
         {
-
-
             int empHour = 0;
             int empWage = 0;
-            Random random = new Random();
-            int empcheck = random.Next(0, 3);
-            switch (empcheck)
+            int totalEmpWage = 0;
+            for (int day = 0; day < Num_Of_Working_Days; day++)
             {
-                case Is_Part_Time:
-                    empHour = 4;
-                    break;
-                case Is_Full_Time:
-                    empHour = 8;
-                    break;
-                default:
-                    empHour = 0;
-                    break;
+                Random random = new Random();
+                int empcheck = random.Next(0, 3);
+                switch (empcheck)
+                {
+                    case Is_Part_Time:
+                        empHour = 4;
+                        break;
+                    case Is_Full_Time:
+                        empHour = 8;
+                        break;
+                    default:
+                        empHour = 0;
+                        break;
+                }
+                empWage = empHour * Emp_Rate_Per_Hour;
+                totalEmpWage = totalEmpWage + empWage;
+                Console.WriteLine("Emp Wage : " + empWage);
             }
-            empWage = empHour * Emp_Rate_Per_Hour;
-            Console.WriteLine("Emp Wage : " + empWage);
 
         }
     }
