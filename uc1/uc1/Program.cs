b4c72eb6c3ad8bcc8a04dc4b6511e4627d43e5cf
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-07-31 17:00:43 +0530

    implement uc1 code clear

---
+++ uc1/uc1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace uc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_PRESENT = 1;
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            if(empcheck == IS_PRESENT)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
